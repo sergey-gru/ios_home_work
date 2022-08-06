cff127ed2d0aeda5e62b8325710d5a8990e1a3de
Sergey Grunenko <grunenko.serg@gmail.com>
2022-08-06 20:43:56 +0400

    Edit 1.2

--- 2. User interface iOS apps/vk_client_test/vk_client_test/Views.swift
+++ 2. User interface iOS apps/vk_client_test/vk_client_test/Views.swift
@@ -5,4 +5,45 @@
 //  Created by admin on 04.08.2022.
 //
 
-import Foundation
+import UIKit
+
+//@IBDesignable
+class MyFrame: UIView {
+    
+    //@IBInspectable
+    
+    override func draw(_ rect: CGRect)
+    {
+        weak var cont = UIGraphicsGetCurrentContext()
+        
+        let r = CGRect(origin: CGPoint(x: 1, y: 1), size: CGSize(width: self.frame.width - 2, height: self.frame.height - 2))
+        let p = UIBezierPath(roundedRect: r, cornerRadius: 15.0).cgPath
+        
+        cont?.clear(rect)
+        //cont?.setFillColor(UIColor.clear.cgColor)
+        //cont?.addRect(r)
+        //cont?.fillPath()
+        
+        
+        cont?.setStrokeColor(self.tintColor.cgColor)
+        cont?.addPath(p)
+        cont?.strokePath()
+        
+        cont?.setFillColor(self.backgroundColor?.cgColor ?? UIColor.white.cgColor)
+        cont?.addPath(p)
+        cont?.fillPath()
+        
+        //cont?.drawPath(using: CGPathDrawingMode.stroke)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        //fatalError("init(coder:) has not been implemented")
+        
+        self.clearsContextBeforeDrawing = false
+        self.isOpaque = false
+        
+        
+    }
+    
+}

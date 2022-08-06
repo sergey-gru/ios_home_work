dec9d52209cc973518878bdfb89d4732a4bbcb4f
Sergey Grunenko <grunenko.serg@gmail.com>
2022-08-06 20:43:12 +0400

    Add lesson 2.1

---
+++ 2. User interface iOS apps/vk_client_test/vk_client_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vk_client_test
+//
+//  Created by admin on 03.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 2. User interface iOS apps/vk_client_test/vk_client_test/Views.swift
@@ -0,0 +1,8 @@
+//
+//  Views.swift
+//  vk_client_test
+//
+//  Created by admin on 04.08.2022.
+//
+
+import Foundation
